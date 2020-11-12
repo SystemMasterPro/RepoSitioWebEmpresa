18c8aa374660010a80dcb2a8a3f48c6b550d7b72
Richard <richardjimenez.9641@gmail.com>
2020-11-12 14:01:11 -0500

    Conexion Blog

---
+++ js/animaciones.js
@@ -0,0 +1,14 @@
+// Mensaje de link faltante
+    function campoVacio() {
+        Swal.fire({
+            title: 'El juego aun no se encuentra disponible!!!',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
+     })
+}
+    
+
